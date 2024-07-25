cdbd877d015fa552dfc7596e4ca2bca1a4a5be47
Bill Wu <71807347+all01280@users.noreply.github.com>
2024-07-25 12:39:44 +0800

    Update filter of trending product

--- src/comp/home.js
+++ src/comp/home.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./home.css";
 import { Link } from "react-router-dom";
+import Homeproduct from "./home_product";
+
+/* Icon */
+import { FaEye, FaHeart } from "react-icons/fa";
 
 const Home = () => {
+  const [trendingProduct, setTrendingProduct] = useState(Homeproduct);
+  // filter of trending product
+  const filtercate = (x) => {
+    const filterproduct = Homeproduct.filter((curElm) => {
+      return curElm.type === x;
+    });
+    setTrendingProduct(filterproduct);
+  };
+  // All Trending Product
+  const allTrendingProduct = () => {
+    setTrendingProduct(Homeproduct);
+  };
+
   return (
     <>
       <div className="home">
@@ -24,9 +41,38 @@ const Home = () => {
                   <h2>trending product</h2>
                 </div>
                 <div className="cate">
-                  <h3>New</h3>
-                  <h3>Featured</h3>
-                  <h3>top selling</h3>
+                  <h3 onClick={() => allTrendingProduct()}>All</h3>
+                  <h3 onClick={() => filtercate("new")}>New</h3>
+                  <h3 onClick={() => filtercate("featured")}>Featured</h3>
+                  <h3 onClick={() => filtercate("top")}>top selling</h3>
+                </div>
+              </div>
+              <div className="products">
+                <div className="container">
+                  {trendingProduct.map((curElm) => {
+                    return (
+                      <>
+                        <div className="box">
+                          <div className="img_box">
+                            <img src={curElm.image} alt="" />
+                            <div className="icon">
+                              <div className="icon_box">
+                                <FaEye />
+                              </div>
+                              <div className="icon_box">
+                                <FaHeart />
+                              </div>
+                            </div>
+                          </div>
+                          <div className="info">
+                            <h3>{curElm.Name}</h3>
+                            <p>${curElm.price}</p>
+                            <button className="btn">Add to cart</button>
+                          </div>
+                        </div>
+                      </>
+                    );
+                  })}
                 </div>
               </div>
             </div>
